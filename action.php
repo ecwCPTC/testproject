cd9b8c58de8dcc1c668bcfcf79611d641499a689
ecwCPTC <103604613+ecwCPTC@users.noreply.github.com>
2022-04-19 09:50:14 -0700

    Add files via upload
    adding php and animated text js

---
+++ action.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+Welcome to my realm, <?php echo $_POST["name"]; ?>.<br>
+Your Social Security Number is: <?php echo $_POST["ssn"]; ?>.<br>
+Your deepest, darkest secret is: <?php echo $_POST["secret"]; ?>.<br>
+Wow. That is a pretty gnarly secret.<br>
+But don't worry. I won't tell anyone.
+
+</body>
+</html>
